7cc58b511f7b0e9e409fde940b98e28b64c35159
RomanSuleimanov <sulroman2006@mail.ru>
2025-08-02 16:05:47 +0700

    Начал делать игру. Сделал выбор сложности игры

---
+++ guesNumberGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guesNumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            int minValue = 0;
+            int maxValue = 100;
+            int guesNumber = random.Next(minValue,maxValue);
+
+            string difficultyLevel;
+            int numberAttempts;
+            
+            Console.WriteLine("Приветствую дорогой друг!!!" +
+                "\nТы попал в игру \"Отгадай число\". Выбери уровень сложности и в отведенное количество ходов отгадай загаданное число программой:");
+            Console.Write("\n- 1.Легкий уровень сложности (20 попыток)" +
+                "\n- 2.Средний уровень сложности (15 попыток)" +
+                "\n- 3.Сложный уровень сложности (10 попыток)");
+            Console.Write("\n\nВыберите уровень сложности : ");
+            difficultyLevel = Console.ReadLine();
+            
+            switch (difficultyLevel)
+            {
+                case "1":
+                    numberAttempts = 20;
+                    Console.WriteLine($"\nПоздравляю вы выбрали \"Легкий уровень сложности\" у вас {numberAttempts} попыток. Хорошей вам игры!!!");
+                    break;
+                case "2":
+                    numberAttempts = 15;
+                    Console.WriteLine($"\nПоздравляю вы выбрали \"Cредний уровень сложности\" у вас {numberAttempts} попыток. Хорошей вам игры!!!");
+                    break;
+                case "3":
+                    numberAttempts = 10;
+                    Console.WriteLine($"\nПоздравляю вы выбрали \"Сложный уровень сложности\" у вас {numberAttempts} попыток. Хорошей вам игры!!!");
+                    break;
+                default:
+                    Console.WriteLine("\nОшибочка, вы ввели не правильный уровень сложности :("); 
+                    Console.WriteLine("Вам нужно ввести число которое обозначает уровень сложности : от 1 до 3");
+                    break;
+            }
+        }
+    }
+}
---
+++ guesNumberGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("guesNumberGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("guesNumberGame")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f6adc3c7-fe45-4a37-ae86-f3ffcc0742c7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
