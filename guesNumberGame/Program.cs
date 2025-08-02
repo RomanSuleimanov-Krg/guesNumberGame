604f9f57252287e697365da6f096cf4b1ed6a845
Mariya Krechmer <sulroman2006@mail.ru>
2025-08-02 19:27:11 +0700

    Сделал основу игры. Осталось допилить правильно отображение кол-ва попыток

--- guesNumberGame/Program.cs
+++ guesNumberGame/Program.cs
@@ -17,7 +17,8 @@ namespace guesNumberGame
             int guesNumber = random.Next(minValue,maxValue);
 
             string difficultyLevel;
-            int numberAttempts;
+            int numberAttempts = 0;
+            int userNumber = 0;
             
             Console.WriteLine("Приветствую дорогой друг!!!" +
                 "\nТы попал в игру \"Отгадай число\". Выбери уровень сложности и в отведенное количество ходов отгадай загаданное число программой:");
@@ -46,6 +47,43 @@ namespace guesNumberGame
                     Console.WriteLine("Вам нужно ввести число которое обозначает уровень сложности : от 1 до 3");
                     break;
             }
+            Console.WriteLine($"И так игра началась. Программа загодала случайное число от о до 100. Предлагайте ваш вариант загадонного числа," +
+                $"\n а программа будет говорить вам на сколько вы близки к отгадыванию. В случае когда кончатся попытки на отгадывание вы проиграете.");
+            
+            for (int i = 0; i < numberAttempts;i++)
+            {
+                if (i >= numberAttempts)
+                {
+                    Console.WriteLine($"Программа загадала число {guesNumber}");
+                    Console.WriteLine($"К сожалению ваши попытки на отгадывание закончились :( . Не расстраивайтесь, у вас обязательно получится в следующий раз");
+                    break;
+                }
+                
+                Console.WriteLine($"У вас осталось {numberAttempts - i} попыток");
+                Console.Write($"Предположите какое число загадала программа? : ");
+                userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if (userNumber == guesNumber)
+                {
+                    Console.WriteLine($"Поздравляю, вы победили!!! Загаданным числом было = {guesNumber}");
+                    break;
+                }
+                else if (userNumber > guesNumber)
+                {
+                    Console.WriteLine($"Загаданное число меньше {userNumber}");
+                    continue;
+                }
+                else if (userNumber < guesNumber)
+                {
+                    Console.WriteLine($"Загаданное число больше {userNumber}");
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Игра окончена. Спасибо вам большое за участие!!!");
+
         }
     }
 }
